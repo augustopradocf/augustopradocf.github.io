2915021beed0023f932f418dab894bd094eaa8aa
Augusto Prado <augustopradof@gmail.com>
2026-03-27 21:36:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const revealedElements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.18,
+  }
+);
+
+revealedElements.forEach((element) => observer.observe(element));
